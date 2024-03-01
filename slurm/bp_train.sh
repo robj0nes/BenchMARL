c004170ee8eea0c57fcec54f0a2648a66d733c8d
Rob <rob64j@hotmail.com>
2024-03-01 08:37:43 +0000

    merge prep

--- slurm/bp_train.sh
+++ slurm/bp_train.sh
@@ -5,14 +5,15 @@
 #SBATCH --mem=50000M
 #SBATCH --partition gpu
 #SBATCH --time-min=7200
-#SBATCH --output ./outputs/gw_gnn_NOshare_500step.stdout
-#SBATCH --error ./outputs/gw_gnn_NOshare_500step.stderr
+#SBATCH --output ./outputs/paint_maddpg_no_share.stdout
+#SBATCH --error ./outputs/paint_maddpg_no_share.stderr
 #SBATCH --job-name=benchmarl_gw
 #SBATCH --account=semt031264
 
 cd "${SLURM_SUBMIT_DIR}"/..
 nvidia-smi
 nvidia-smi mig -lgip
-python benchmarl/run.py algorithm=mappo experiment.share_policy_params=False task=vmas/give_way model=layers/gnn
-# python benchmarl/run.py algorithm=mappo task=vmas/give_way model=layers/gnn
-# python train/train_multi_give_way.py
+# python benchmarl/run.py algorithm=mappo experiment.share_policy_params=False experiment.lr=0.005 task=vmas/painting task.n_goals=4 model=layers/gnn
+python benchmarl/run.py algorithm=maddpg experiment.share_policy_params=False experiment.lr=0.005 task=vmas/painting task.n_goals=4 model=layers/gnn
+# python benchmarl/run.py algorithm=maddpg experiment.share_policy_params=False experiment.lr=0.005 task=vmas/painting task.n_goals=4 model=layers/gnn +task.position_shaping_factor=5.0
+
