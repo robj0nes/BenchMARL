32d88ee1083dbc253ce4ec505a0dcadf5552d151
Rob <rob64j@hotmail.com>
2024-09-05 11:37:46 +0100

    Updating for BP

--- slurm/batch_launcher.sh
+++ slurm/batch_launcher.sh
@@ -1,29 +1,34 @@
-export CONFIG='experiment=bcp4_experiment'
-export MODEL='model=layers/gnn'
+export CONFIG='experiment=bp_experiment'
+export AGENT_MODEL='model=layers/gnn'
+export CRITIC_MODEL='model@critic_model=layers/deepsets'
 
 # for ALGO in mappo maddpg masac ippo iddpg iql isac qmix vdn
 for ALGO in maddpg
+export ALGO
 do
-  for SHARE_PARAMS in True False
+  for SEED in {0..4}
   do
-    export EXP="experiment.share_policy_params="${SHARE_PARAMS}""
-    # No critic in IQL, VDN and QMIX
-    if [ "${ALGO}" == "vdn" ] || [ "${ALGO}" == "iql" ] || [ "${ALGO}" == "qmix" ]
-    then
-      export OUTPUT="./outputs/"${ALGO}"_sp"${SHARE_PARAMS}"_scFalse"
-      export ALGO
-      export ALGO_CONF=''
-      sbatch -o "${OUTPUT}.stdout" -e "${OUTPUT}.stderr" ./bcp4_train.sh 
-    #   echo python benchmarl/run.py "${CONFIG}" algorithm="${ALGO}" task=vmas/painting "${MODEL}" experiment.share_policy_params="${SHARE_PARAMS}"
-    else
-      for SHARE_CRITIC in True False
-      do
-        export OUTPUT="./outputs/"${ALGO}"_sp"${SHARE_PARAMS}"_sc"${SHARE_CRITIC}""
-        export ALGO
-        export ALGO_CONF="algorithm.share_param_critic="${SHARE_CRITIC}""
-        sbatch -o "${OUTPUT}.stdout" -e "${OUTPUT}.stderr" ./bcp4_train.sh 
-        # echo python benchmarl/run.py "${CONFIG}" algorithm="${ALGO}" task=vmas/painting "${MODEL}" experiment.share_policy_params="${SHARE_PARAMS}" algorithm.share_param_critic="${SHARE_CRITIC}"
-      done
-    fi 
-  done    
+    export SEED
+    for SHARE_PARAMS in False
+    do
+      export EXP="experiment.share_policy_params="${SHARE_PARAMS}""
+      # No critic in IQL, VDN and QMIX
+      if [ "${ALGO}" == "vdn" ] || [ "${ALGO}" == "iql" ] || [ "${ALGO}" == "qmix" ]
+      then
+        export OUTPUT="./outputs/"${ALGO}"_sp"${SHARE_PARAMS}"_scFalse_seed"${SEED}"" 
+        export ALGO_CONF=''
+        # sbatch -o "${OUTPUT}.stdout" -e "${OUTPUT}.stderr" ./bp_train.sh 
+      #   echo python benchmarl/run.py "${CONFIG}" algorithm="${ALGO}" task=vmas/painting "${MODEL}" experiment.share_policy_params="${SHARE_PARAMS}"
+      else
+        for SHARE_CRITIC in True
+        do
+          export OUTPUT="./outputs/"${ALGO}"_sp"${SHARE_PARAMS}"_sc"${SHARE_CRITIC}"_seed"${SEED}""
+          export ALGO_CONF="algorithm.share_param_critic="${SHARE_CRITIC}""
+          sbatch -o "${OUTPUT}.stdout" -e "${OUTPUT}.stderr" ./bp_train.sh 
+          # echo benchmarl/run.py seed="${SEED}" "${CONFIG}" "${EXP}" "${AGENT_MODEL}" "${CRITIC_MODEL}" algorithm="${ALGO}" "${ALGO_CONF}" task=vmas/painting 
+          # echo python benchmarl/run.py "${CONFIG}" algorithm="${ALGO}" task=vmas/painting "${MODEL}" experiment.share_policy_params="${SHARE_PARAMS}" algorithm.share_param_critic="${SHARE_CRITIC}"
+        done
+      fi 
+    done 
+  done     
 done 
--- slurm/bp_train.sh
+++ slurm/bp_train.sh
@@ -1,31 +1,33 @@
-#!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --gres=gpu:1
-##SBATCH --gpus-per-node=1
-#SBATCH --mem=50000M
+#!/bin/bash -login
+#SBATCH --job-name benchmarl
+#SBATCH --nodes 1
+#SBATCH --gpus=1
 #SBATCH --partition gpu
-#SBATCH --time-min=7200
-#SBATCH --output ./outputs/paint_cutsomCritic_maddpg_noShare_2a-2g.stdout
-#SBATCH --error ./outputs/paint_cutsomCritic_maddpg_noShare_2a-2g.stderr
-#SBATCH --job-name=benchmarl_paint
+#SBATCH --mem=50000M
 #SBATCH --account=semt031264
 
+module load cuda
+conda activate benchmarl
+
 cd "${SLURM_SUBMIT_DIR}"/..
+
+echo Running on host `hostname`
 nvidia-smi
-nvidia-smi mig -lgip
+# nvidia-smi mig -lgip
+echo Time is `date`
+echo Directory is `pwd`
+echo Slurm job ID is $SLURM_JOB_ID
+echo This job runs on the following machines:
+echo `echo $SLURM_JOB_NODELIST | uniq`
 
-CONFIG="algorithm=maddpg \
-        algorithm.share_param_critic=False \
-        experiment=base_experiment \
-        experiment.sampling_device=cuda \
-        experiment.train_device=cuda \
-        experiment.share_policy_params=False \
-        experiment.lr=0.0005 \
-        task=vmas/painting \
-        task.n_goals=2 \
-        task.n_agents=2 \
-        task.pos_shaping=True \
-        model=layers/gnn \
-        model@critic_model=critic_seq"
+# These values are exported from the batch_launcher script
+echo Seed = "${SEED}"
+echo Config = "${CONFIG}"
+echo Agent Model = "${AGENT_MODEL}"
+echo Critic Model = "${CRITIC_MODEL}"
+echo Algorithm = "${ALGO}"
+echo Algorithm Config = "${ALGO_CONF}"
+echo Experiment Config = "${EXP}"
 
-python benchmarl/run.py ${CONFIG}
+python benchmarl/run.py seed=0 experiment=bp_experiment model=layers/gnn model@critic_model=layers/deepsets algorithm=maddpg task=vmas/painting
+# python benchmarl/run.py seed="${SEED}" "${CONFIG}" "${EXP}" "${AGENT_MODEL}" "${CRITIC_MODEL}" algorithm="${ALGO}" "${ALGO_CONF}" task=vmas/painting 
